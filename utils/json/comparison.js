8ee052e4d6e5efe7db3a28e4231cc62e69342298
Sed21 <tungstensteel@protonmail.com>
2021-04-29 20:03:25 +0300

    ISP-77 Fixed JSON comparison for nested objects.

--- utils/json/comparison.js
+++ utils/json/comparison.js
@@ -3,6 +3,26 @@ const arraysEqual = function (a, b) {
         a.length === b.length
 }
 
+const isObject = val => typeof val === 'object' && !Array.isArray(val);
+
+
+/**
+ * Get all JSON objects key or values as array
+ */
+function getObjectFields(obj, getter = "key") {
+    const paths = (obj = {}) => {
+        return Object.entries(obj)
+            .reduce((product, [key, value]) =>
+            {
+                return isObject(value) ?
+                    product.concat(paths(value))
+                    : product.concat(getter === "key" ? key : value)
+            }, []);
+    }
+    return paths(obj);
+}
+
+
 /**
  * Compare JSON objects by keys and values
  * @param refObj
@@ -10,8 +30,8 @@ const arraysEqual = function (a, b) {
  * @returns {boolean}
  */
 function compareByValues(refObj, cmpObj) {
-    return arraysEqual(Object.keys(refObj), Object.keys(cmpObj)) &&
-            arraysEqual(Object.values(refObj), Object.values(cmpObj))
+    return arraysEqual(getObjectFields(refObj, "key"), getObjectFields(cmpObj, "key")) &&
+            arraysEqual(getObjectFields(refObj, "value"), getObjectFields(cmpObj, "value"))
 }
 
 
@@ -22,8 +42,8 @@ function compareByValues(refObj, cmpObj) {
  * @returns {boolean}
  */
 function compareByTypes(refObj, cmpObj) {
-    const refObjValuesTypes = Object.values(refObj).map((a) => typeof a)
-    const cmpObjValuesTypes = Object.values(cmpObj).map((a) => typeof a)
+    const refObjValuesTypes = getObjectFields(refObj, "value").map((a) => typeof a)
+    const cmpObjValuesTypes = getObjectFields(cmpObj, "value").map((a) => typeof a)
 
     return arraysEqual(Object.keys(refObj), Object.keys(cmpObj)) &&
             arraysEqual(refObjValuesTypes, cmpObjValuesTypes)
