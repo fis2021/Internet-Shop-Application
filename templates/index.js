7a1130063987a40189f65627ceb96ec604e2c956
Sed21 <tungstensteel@protonmail.com>
2021-05-13 18:26:29 +0300

    ISP-39 Add request body templates for adding funds.

--- templates/index.js
+++ templates/index.js
@@ -2,10 +2,12 @@ const registerTemplates = require('./auth/register')
 const loginTemplate = require('./auth/login')
 const logoutTemplate = require('./auth/logout')
 const actionsTemplate = require('./products/actions')
+const customerTemplate = require('./customer/funds')
 
 module.exports = {
     register : registerTemplates, // Contains registration requests JSON templates for each user type
     login : loginTemplate,
     logout : logoutTemplate,
-    action : actionsTemplate
+    action : actionsTemplate,
+    customer : customerTemplate
 }
