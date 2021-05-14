28bd9a23b7628e9cb9e6510fd4492523d3fcedc0
Sed21 <tungstensteel@protonmail.com>
2021-05-14 16:09:36 +0300

    ISP-47 Fixed invalid request parameters proccessing.

--- routes/customer/carts.js
+++ routes/customer/carts.js
@@ -47,36 +47,44 @@ router.post('/changes', async function(req, res, next) {
     const userCartQuery = await database.query(`SELECT cart_content, cart_total_cost FROM ${database.Tables.carts} 
                                                 WHERE cart_owner_uuid = $1;`, [customerUUID])
 
-    const productInfo = productQuery.rows[0]
+    const productInfo = productQuery.rows
+    if(productInfo.length <= 0){
+        res.status(400).end()
+        return
+    }
     if(userCartQuery.rows.length <= 0){
         let obj = []
         if(!isNaN(Math.abs(Number(quantity)))){
             obj[itemUUID] = quantity
+        } else {
+            res.status(400).end()
+            return
         }
         const createCart = `INSERT INTO ${database.Tables.carts} (
                                 cart_owner_uuid,
                                 cart_content,
                                 cart_total_cost) 
                             VALUES ($1, ARRAY ['${JSON.stringify(obj)}'], $2) RETURNING true;`
-        const dbReponse = await database.query(createCart, [customerUUID, obj[itemUUID] === undefined ? 0 : obj[itemUUID]])
+        const newCartCost = obj[itemUUID] === undefined ? 0 : Number(obj[itemUUID]) * Number(quantity)
+        const dbReponse = await database.query(createCart, [customerUUID, newCartCost] )
         if(dbReponse.rows.length > 0){
-            res.json({"message" : "Item added."})
+            res.json({"message" : "Request processed successfully."})
         } else {
             res.status(500).end()
         }
         return
     }
-    if(productInfo.length <= 0){
-        res.status(400).end()
-        return
-    }
 
     let updatedCart = JSON.parse(userCartQuery.rows[0].cart_content)
+    let updatedCartTotalCost = Number(userCartQuery.rows[0]['cart_total_cost'])
 
+
+    console.log(updatedCart)
     switch (action) {
         case "add" :
             if(!isNaN(Math.abs(Number(quantity)))){
                 updatedCart[itemUUID] = Number(updatedCart[itemUUID] === undefined ? 0 : updatedCart[itemUUID]) + Number(quantity)
+                updatedCartTotalCost += Number(quantity) * productInfo[0].product_price
             } else {
                 res.status(400).end()
                 return
@@ -85,14 +93,18 @@ router.post('/changes', async function(req, res, next) {
         case "remove" :
             if(quantity.toLowerCase() === "all"){
                 updatedCart = {}
+                updatedCartTotalCost = 0
             } else if(!isNaN(Math.abs(Number(quantity)))){
                 let upd = Number(updatedCart[itemUUID] === undefined ? 0 : updatedCart[itemUUID]) - Number(quantity)
                 if(upd <= 0){
+                    updatedCartTotalCost -= Number(updatedCart[itemUUID]) * productInfo[0].product_price
                     delete updatedCart[itemUUID]
                 } else if(!isNaN(Number(updatedCart[itemUUID]))){
                     updatedCart[itemUUID] = Number(updatedCart[itemUUID]) - Number(isNaN(Math.abs(Number(quantity))) ? 0 : Number(quantity))
+                    updatedCartTotalCost  -= Number(quantity) * productInfo[0].product_price
                 } else {
                     updatedCart = {}
+                    updatedCartTotalCost = 0
                 }
             } else {
                 res.status(400).end()
@@ -105,18 +117,14 @@ router.post('/changes', async function(req, res, next) {
     }
 
     const updateCartStmt = `UPDATE ${database.Tables.carts} SET 
-                                cart_content = ARRAY ['${JSON.stringify(updatedCart)}'], 
+                                cart_content = ARRAY ['${JSON.stringify({...updatedCart})}'], 
                                 cart_total_cost = $1 
                             WHERE cart_owner_uuid = $2 RETURNING TRUE;`
 
-    const totalCartCost = quantity === "All" || Object.keys(updatedCart).length === 0 ? 0 : Object.values(updatedCart).reduce((a, b) => a + b)
+    const totalCartCost = quantity === "All" || Object.keys(updatedCart).length === 0 ? 0 : updatedCartTotalCost
     const dbResponse = await database.query(updateCartStmt, [totalCartCost, customerUUID])
     if(dbResponse.rows.length > 0){
-        console.log(updatedCart)
-        res.json({
-            "totalCost" : totalCartCost,
-            "cart" : updatedCart
-        })
+        res.json({"message" : "Request processed successfully."})
     } else {
         res.status(500).end()
     }
@@ -126,6 +134,5 @@ router.post('/changes', async function(req, res, next) {
 router.get('/view', async function(req, res, next) {
     res.json("You reached the root page of API")
 })
-
 
 module.exports = router
