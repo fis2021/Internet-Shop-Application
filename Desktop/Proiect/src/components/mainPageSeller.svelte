00d39e82d9a57ed899b1ecf94edbc5143b91d47b
segiu111 <segiu.bunea@student.upt.ro>
2021-05-15 17:38:21 +0300

    ISP-28 Update mainPageSeller

--- Desktop/Proiect/src/components/mainPageSeller.svelte
+++ Desktop/Proiect/src/components/mainPageSeller.svelte
@@ -1,29 +1,37 @@
 <script>
     export let menu = 1;
 
-    let spiderVerse = [
-       {name: 'Spider-Gwen', id: 'Gwen Stacy'},
-       {name: 'Spider-Man', id: 'Miles Morales'},
-       {name: 'Spider-Ham', id: 'Peter Porker'}
-    ]       
+    let company = [
+        {image:'img1', nameProduct:'product1', price: 20, id: 1},
+        {image:'img2', nameProduct:'product2', price: 30, id: 2},
+        {image:'img3', nameProduct:'product3', price: 25, id: 3}
+    ];
+
+    const handleClick = (id) => {
+        company = company.filter((cmp) => cmp.id != id);
+    }
 
 </script>
 
 <nav class="navbar" role="navigation" method="post">
     <div class="navbar-menu">
-        <a href="#/mainPageSeller"  class="mainPageSeller" on:click|preventDefault={() => (menu = 1)}><h1>Seller mainpage</h1>Seller mainpage</a>
+        <a href="#/mainPageSeller"  class="mainPageSeller" on:click|preventDefault={() => (menu = 1)}>Seller mainpage</a>
         <button class="button-seller"><a href="#/myAccountPageSeller" class="register-seller">My account</a></button>
     </div>
 </nav>
 
 {#if menu === 1}
-
-  <h1>SpiderVerse</h1>
-  <ul>
-    {#each spiderVerse as spider}
-      <li>{spider.name}</li>
-    {/each}
-  </ul>
+    <div class="display">
+        {#each company as cmp (cmp.id)}
+            <div class="seller">
+                <p>Image: {cmp.image}</p>
+                <p>Product's name: {cmp.nameProduct}</p>
+                <p>Price: {cmp.price}</p>
+                <button class="button-delete" on:click={() => handleClick(cmp.id)}>DELETE</button>
+                <button class="button-edit"><a href="#/editPage" class="edit">EDIT</a></button>
+            </div>
+        {/each}
+    </div>
 {/if}
 
 <style>
@@ -34,6 +42,13 @@
    .mainPageSeller{
         color: #274472;
         text-decoration:none;
+    }
+
+    .display{
+        display: grid;
+        margin: 0 50px;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
     }
 
     .button-seller{
@@ -48,6 +63,42 @@
     }
 
     .register-seller{
+        background: #274472;
+        color: white;
+        text-decoration:none;
+    }
+
+    .seller{
+        padding:20px;
+        background: white;
+        border-radius: 6px;
+        box-shadow: 0px 2px 4px;
+    }
+
+    .button-delete{
+        background: #274472;
+        color: white;
+        text-transform: uppercase;
+        border-radius: 25px;
+        width: 150px;
+        font-weight: bold;
+        cursor: pointer;
+        transition: all 300ms ease-in-out;  
+    }
+
+    .button-edit{
+        background: #274472;
+        color: white;
+        text-transform: uppercase;
+        border-radius: 25px;
+        width: 150px;
+        font-weight: bold;
+        cursor: pointer;
+        transition: all 300ms ease-in-out;
+        margin-left: 15px;
+    }
+
+    .edit{
         background: #274472;
         color: white;
         text-decoration:none;
